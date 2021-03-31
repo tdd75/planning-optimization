04c6a6f747d318f16469ad77665ea478cd2817d9
Duy Tran <tranducduy7520@gmail.com>
2021-03-31 16:54:26 +0700

    update

--- Queen-Heuristic.py
+++ Queen-Heuristic.py
@@ -1,37 +1,97 @@
-def check(v, k):
-    # check if v can be assigned to x[k] without violating the constraints
-    return mr[v] == False and md1[v + k] == False and md2[v - k + n - 1] == False
-
-def Solution():
-    global found
-    found = True
-    print('FOUND')
-    print(x)
+import random as rd
 
 
-def Try(k):
-    if found:
-        return
-    for v in range(n):
-        if check(v, k):
-            x[k] = v
-            mr[v] = True
-            md1[x[k] + k] = True
-            md2[x[k] - k + n - 1] = True
-            if k == n - 1:
-                Solution()
-            else:
-                Try(k + 1)
-            # recover then backtracking
-            mr[v] = False
-            md1[x[k] + k] = False
-            md2[x[k] - k + n - 1] = False
+def violations():
+    v = 0
+    for i in range(n-1):
+        for j in range(i + 1, n):
+            if x[i] == x[j]:
+                v += 1
+            if x[i] + i == x[j] + j:
+                v += 1
+            if x[i] - i == x[j] - j:
+                v += 1
+    return v
 
-found = False
-n = 6
+
+def generate_initial_solution():
+    for i in range(n):
+        x[i] = 0
+
+
+def violations_q(q):
+    v = 0
+    for i in range(n):
+        if i != q:
+            if x[i] == x[q]:
+                v += 1
+            if x[i] + i == x[q] + q:
+                v += 1
+            if x[i] - i == x[q] - q:
+                v += 1
+    return v
+
+
+def select_most_violating_queen():
+    sel_q = 1
+    max_violations = 0
+    cand = []
+    for q in range(n):
+        vq = violations_q(q)
+        if max_violations < vq:
+            max_violations = vq
+            cand.clear()
+            cand.append(q)
+        elif max_violations == vq:
+            cand.append(q)
+
+    idx = rd.randint(0, len(cand) - 1)
+    sel_q = cand[idx]
+    return sel_q
+
+
+def violations_q_r(q, r):
+    # return the violations if x(q) is assigned to r
+    old = x[q]
+    x[q] = r
+    v = violations()
+    x[q] = old
+    return v
+
+
+def select_most_promissing_row(q):
+    min_violations = 1e9
+    sel_r = -1
+    cand = []
+    for r in range(n):
+        vr = violations_q_r(q, r)
+        if min_violations > vr:
+            min_violations = vr
+            cand.clear()
+            cand.append(r)
+        elif min_violations == vr:
+            cand.append(r)
+
+    idx = rd.randint(0, len(cand) - 1)
+    sel_r = cand[idx]
+    return sel_r
+
+
+def solve():
+    generate_initial_solution()
+
+    for k in range(10000):
+        q = select_most_violating_queen()
+        v = select_most_promissing_row(q)
+        x[q] = v
+        v = violations()
+        print('step', k, 'assign x[', q, '] = ',
+              v, 'violations = ', violations())
+        if v == 0:
+            print(x)
+            break
+
+
+n = 10
 x = [0 for i in range(n)]
-mr = [False for i in range(n)]
-md1 = [False for i in range(0, 2 * n - 1)]
-md2 = [False for i in range(0, 2 * n - 1)]
-
-Try(0)
+solve()
--- TSP-branch-and-bound.py
+++ TSP-branch-and-bound.py
@@ -4,20 +4,23 @@ import pandas as pd
 
 cmin = 1e9
 
-def input(filename):
-	global N,c
-	with open(filename) as f:
-		for line in f:
-			N = int(line) # read the number of points
-			print('N = ',N)
-			break
-			
-		c = [[int(num) for num in line.split()] for line in f] # read the whole distance matrix
-		
-	return N,c
 
-N, c = input('input.txt')
-print(N,c)
+def input(filename):
+    global N, c
+    with open(filename) as f:
+        for line in f:
+            N = int(line)  # read the number of points
+            print('N = ', N)
+            break
+
+        c = [[int(num) for num in line.split()]
+             for line in f]  # read the whole distance matrix
+
+    return N, c
+
+
+N, c = input('TSP-4.txt')
+print(N, c)
 
 x = [0 for i in range(N)]
 visited = [False for i in range(N)]
@@ -26,49 +29,54 @@ f_best = 1e9
 
 
 def SortedCandidates(cur):
-	cand = []
-	for v in range(N):
-		if visited[v] == False:
-			cand.append(v)
-	
-	cand.sort(key = lambda x: c[cur][x]) # sort elements in cand in an increasing order of the distance from cur
-	return cand
-	
+    cand = []
+    for v in range(N):
+        if visited[v] == False:
+            cand.append(v)
+
+    # sort elements in cand in an increasing order of the distance from cur
+    cand.sort(key=lambda x: c[cur][x])
+    return cand
+
+
 def updateBest():
-	global f_best
-	
-	if f + c[x[N-1]][x[0]] < f_best:
-		f_best = f + c[x[N-1]][x[0]]
-		print('update best ',f_best)
-		
-def Try(k): # try all values for x[k], being aware x[0],x[1],...,x[k-1], length = f
-	global f,x,visited
-	
-	cand = SortedCandidates(x[k-1])
-	#print('Try(',k,' cand = ',cand)
-	for v in cand:
-		x[k] = v
-		f = f + c[x[k-1]][x[k]]
-		visited[v] = True
-		
-		if k == N-1:
-			updateBest()
-		else:
-			if f + cmin*(N-k) < f_best:
-				Try(k+1)
-				
-		visited[v] = False # recover when backtracking
-		f = f - c[x[k-1]][x[k]]
-	
+    global f_best
+
+    if f + c[x[N-1]][x[0]] < f_best:
+        f_best = f + c[x[N-1]][x[0]]
+        print('update best ', f_best)
+
+
+# try all values for x[k], being aware x[0],x[1],...,x[k-1], length = f
+def Try(k):
+    global f, x, visited
+
+    cand = SortedCandidates(x[k-1])
+    #print('Try(',k,' cand = ',cand)
+    for v in cand:
+        x[k] = v
+        f = f + c[x[k-1]][x[k]]
+        visited[v] = True
+
+        if k == N-1:
+            updateBest()
+        else:
+            if f + cmin*(N-k) < f_best:
+                Try(k+1)
+
+        visited[v] = False  # recover when backtracking
+        f = f - c[x[k-1]][x[k]]
+
+
 for i in range(N):
-	for j in range(N):
-		if i != j and cmin > c[i][j]:
-			cmin = c[i][j]
+    for j in range(N):
+        if i != j and cmin > c[i][j]:
+            cmin = c[i][j]
 
 x[0] = 0
 visited[0] = True
 Try(1)
-			
-f = open('result.txt','w')
+
+f = open('result.txt', 'w')
 f.write(str(f_best))
-f.close()			
+f.close()
